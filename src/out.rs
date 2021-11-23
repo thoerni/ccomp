b36f928ca0a053e63aa12af8a9a31a98b45bc58c
Max Thorn <maxpaul.thorn@gmail.com>
2021-11-23 23:12:16 +0100

    added third party library for easier color hanlding

--- src/out.rs
+++ src/out.rs
@@ -1,26 +1,13 @@
-pub static TERMINAL_RED: &str = "\u{001b}[31m";
-pub static TERMINAL_GREEN: &str = "\u{001b}[32m";
-pub static TERMINAL_BOLD: &str = "\033[31m";
-pub static TERMINAL_STOP: &str = "\u{001b}[0m";
-
+use colored::*;
 
 pub fn status(message: &str) {
-    println!(
-        "{bold}{green}➜{stop} {bold}{message}{stop}",
-        message = message,
-        bold = TERMINAL_BOLD,
-        green = TERMINAL_GREEN,
-        stop = TERMINAL_STOP
-    );
+    let arrow = "➜".bold().green();
+    let message = message.bold();
+    println!("{} {}", arrow, message);
 }
 
 pub fn error(message: &str) {
-    eprintln!(
-        " {bold}{red}➜{stop} {bold}{message}{stop}",
-        message = message,
-        bold = TERMINAL_BOLD,
-        red = TERMINAL_RED,
-        stop = TERMINAL_STOP
-    );
-
+    let arrow = "➜".bold().red();
+    let message = message.bold();
+    eprintln!("{} {}", arrow, message);
 }
